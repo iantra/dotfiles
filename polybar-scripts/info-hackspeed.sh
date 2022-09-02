4a79801699e2bb54645ddfbcd818035ab68998dd
iantra <iantrasolari@gmail.com>
2022-09-02 16:22:06 +0200

    Fix custom scripts

--- polybar-scripts/info-hackspeed.sh
+++ polybar-scripts/info-hackspeed.sh
@@ -2,12 +2,12 @@
 #!/bin/sh
 # shellcheck disable=SC2016,SC2059
 
-KEYBOARD_ID="AT Translated Set 2 keyboard"
+KEYBOARD_ID="DZTECH DZ60RGB Keyboard"
 
 # cpm: characters per minute
 # wpm: words per minute (1 word = 5 characters)
 METRIC=cpm
-FORMAT="# %d $METRIC"
+FORMAT="⌨️ %d $METRIC"
 
 INTERVAL=20
 
